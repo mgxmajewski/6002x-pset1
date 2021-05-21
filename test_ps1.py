2ca1159373aa5726765539504b100a21c1f8ccbd
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-21 23:54:37 +0200

    Create test class to test partition_enumerator

--- test_ps1.py
+++ test_ps1.py
@@ -1,3 +1,18 @@
+from ps1 import partition_enumerator
+from assertpy import assert_that
+
 class TestBruteForceCowsTransport:
     def test_brute_force_cow_transport(self):
         assert False
+
+
+class TestPartitionEnumerator:
+    def test_partition_enumerator(self):
+        # given
+        partitions = [["a", "b"], ["c", "d"]]
+        expected = [(1 ,["a", "b"]), (2, ["c", "d"])]
+        # when
+        result = partition_enumerator(partitions)
+        # then
+        assert_that(result).is_equal_to(expected)
+
