5806098451a72fb4e6c812315dcf00e7be63258f
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-22 02:43:25 +0200

    Create TestClass for partition space evaluation

--- test_ps1.py
+++ test_ps1.py
@@ -1,4 +1,5 @@
-from ps1 import partition_enumerator
+from ps1 import partition_enumerator, partition_eval
+import pytest
 from assertpy import assert_that
 
 class TestBruteForceCowsTransport:
@@ -16,3 +17,20 @@ class TestPartitionEnumerator:
         # then
         assert_that(result).is_equal_to(expected)
 
+
+class TestPartitionEval:
+
+    @pytest.fixture(autouse=True)
+    def prepare_partition_eval(self):
+        self.partition_eval = partition_eval
+
+    def test_partition_eval(self):
+        # given
+        spaces_menu = {'Maggie': 3, 'Herman': 7, 'Betsy': 9, 'Oreo': 6, 'Moo Moo': 3,
+                       'Milkshake': 2, 'Millie': 5, 'Lola': 2, 'Florence': 2, 'Henrietta': 9}
+        test_partition = [(12, ['Betsy', 'Henrietta'])]
+        expected = [(12, 18)]
+        # when
+        result = self.partition_eval(spaces_menu, test_partition)
+        # then
+        assert_that(result).is_equal_to(expected)
